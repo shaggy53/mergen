38cf220468537273b58c67ed60c0033e2af5a64e
Selçuk <selcuk@mollaibrahimoglu.com>
2019-01-28 01:29:01 +0300

    Update SiteController.php

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -4,8 +4,6 @@ function getIndex(){
 	echo 'controllere geldin!(getindex/sitecontroller)';
 }
 function findUri($inputs){
-	print_r($inputs);
-	exit;
   view('pages.index',$inputs);
 }
 function findUri2($post){
